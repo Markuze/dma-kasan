6735c0c92434d8127a5b19596e507c3ab5ea8db6
Build VM <build@example.com>
2020-05-29 15:54:17 +0000

    create image fixup

--- create_image.sh
+++ create_image.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential libncurses5-dev gcc make git bc libssl-dev libelf-dev libreadline-dev binutils-dev libnl-genl-3-dev make trace-cmd
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y flex bison
+	sudo apt-get update
+	sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential libncurses5-dev gcc make git bc libssl-dev libelf-dev libreadline-dev binutils-dev libnl-genl-3-dev make trace-cmd
+	sudo DEBIAN_FRONTEND=noninteractive apt-get install -y flex bison
 
 path=`realpath $0`
 path=`dirname $path`
@@ -14,7 +14,7 @@ git clone git://kernel.ubuntu.com/ubuntu/ubuntu-bionic.git --depth=1 --branch=v5
 cd ubuntu-bionic
 
 echo "cp .config"
-sudo cp /boot/config-`uname -r` .config
+udo cp /boot/config-`uname -r` .config
 sudo chown `whoami` .config
 
 echo "apply patches"
@@ -29,9 +29,14 @@ git am -3 $path/*.patch
 #scripts/kconfig/streamline_config.pl > modconfig
 #[ ! "$?" -eq "0" ] && exit 4
 #mv modconfig .config
+./scripts/config --enable CONFIG_KASAN
 make olddefconfig
 [ ! "$?" -eq "0" ] && exit 1
 ./scripts/config --disable CONFIG_XFS_ONLINE_SCRUB
+./scripts/config --disable KASAN_EXTRA
+./scripts/config --enable CONFIG_KASAN_OUTLINE
+
+#make localmodconfig
 
 #echo "build image"
 #cp ./debian/scripts/retpoline-extract-one ./scripts/ubuntu-retpoline-extract-one
@@ -43,7 +48,7 @@ make olddefconfig
 #sed -i 's/obj-y/obj-n/g' ubuntu/Makefile
 
 #make -j `nproc` LOCALVERSION=-cbn > /dev/null
-make -j `nproc` > /dev/null
+make -j `nproc` #> /dev/null
 [ ! "$?" -eq "0" ] && exit 2
 
 echo "install image"
@@ -51,17 +56,17 @@ sudo DEBIAN_FRONTEND=noninteractive make modules_install install
 #sudo DEBIAN_FRONTEND=noninteractive make INSTALL_MOD_STRIP=1 modules_install install
 [ ! "$?" -eq "0" ] && exit 3
 
-exit
+#exit
 
 echo 'configuring grub'
-sudo sh -c 'echo GRUB_CMDLINE_LINUX=\"\$GRUB_CMDLINE_LINUX threadirqs\" >> /etc/default/grub.d/50-cloudimg-settings.cfg'
-sudo sh -c 'echo GRUB_CMDLINE_LINUX=\"\$GRUB_CMDLINE_LINUX threadirqs\" >> /etc/default/grub'
+#sudo sh -c 'echo GRUB_CMDLINE_LINUX=\"\$GRUB_CMDLINE_LINUX threadirqs\" >> /etc/default/grub.d/50-cloudimg-settings.cfg'
+#sudo sh -c 'echo GRUB_CMDLINE_LINUX=\"\$GRUB_CMDLINE_LINUX threadirqs\" >> /etc/default/grub'
 
-string='cbn'
-STR=`sudo grep gnulinux /boot/grub/grub.cfg |grep -v recovery|grep $string|awk '{print $(NF-1)}'`
-echo $STR
-sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=$STR/" /etc/default/grub
-sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=$STR/" /etc/default/grub.d/50-cloudimg-settings.cfg
+#string='cbn'
+#STR=`sudo grep gnulinux /boot/grub/grub.cfg |grep -v recovery|grep $string|awk '{print $(NF-1)}'`
+#echo $STR
+#sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=$STR/" /etc/default/grub
+#sudo sed -i "s/GRUB_DEFAULT=.*/GRUB_DEFAULT=$STR/" /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo sh -c 'echo GRUB_DISABLE_RECOVERY="true" >> /etc/default/grub'
 sudo sh -c 'echo GRUB_DISABLE_SUBMENU=y >> /etc/default/grub'
 sudo update-grub2
